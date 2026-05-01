21ff08b8125c2653320228fa05ee09e5dd7c37e6
elie.ravoux <elie.ravoux@gmail.com>
2026-05-01 12:39:03 +0200

    Feat : Finalisation de la détection de plusieurs markers

--- Assets/Scripts/ARManagerBridge.cs
+++ Assets/Scripts/ARManagerBridge.cs
@@ -22,11 +22,19 @@ public class ARManagerBridge : MonoBehaviour
     }
 
     void OnImagesChanged(ARTrackedImagesChangedEventArgs args)
+{
+    Debug.Log("Le Manager a détecté un changement ! Nombre d'images : " + args.updated.Count);
+    
+    foreach (var image in args.updated)
     {
-        foreach (var image in args.updated)
+        
+        Debug.Log("DANS LE FOR : " + args.updated.Count);
+        // On ne traite l'image que si elle est actuellement "Tracking" (bien vue par la caméra)
+        if (image.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Tracking)
         {
-            // On envoie le nom de l'image et l'objet image au Visual Scripting
+            Debug.Log("DANS LE IF : " + args.updated.Count);
             CustomEvent.Trigger(gameObject, "MarkerUpdated", image.referenceImage.name, image.transform.position, image.transform.rotation);
         }
     }
+}
 }
