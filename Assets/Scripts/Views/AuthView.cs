1f2a6d241a9cb466e09040fbcbeb479099b52a2f
Lisa <lisa.quantin@gmail.com>
2026-05-06 19:21:40 +0200

    Fix : authentification d'ont appear when you change pages

--- Assets/Scripts/Views/AuthView.cs
+++ Assets/Scripts/Views/AuthView.cs
@@ -48,7 +48,11 @@ public class AuthView : MonoBehaviour
 
     private void OnAuthStateChanged(bool isLoggedIn, string userId, string email)
     {
-        UpdateUI();
+        // Don't do anything
+        if (0==1)
+        {
+            UpdateUI();
+        }
     }
 
     private void UpdateUI()
